1b7b04c085147820ab35b97b3bdf5c4341b7fe39
priyankavrao97 <prao1@mail.csuchico.edu>
2023-03-28 10:41:18 -0700

    Code coverage changes

--- src/Roller.test.ts
+++ src/Roller.test.ts
@@ -4,9 +4,32 @@ describe("Smoke test", ()=> {
   test("The test scaffold runs successfully.", ()=> {
     expect(true).toBe(true);
   });
-})
+});
 
 describe("Roller tests", ()=> {
   test("Description", () => {
   });
 });
+
+test("Faces of die", () => {
+  let roller: Roller = new Roller(-1);  
+  expect(6).toBe(6);
+});
+
+test("Faces of die", () => {
+  let roller: Roller = new Roller(4);  
+  expect(4).toBe(4);
+});
+
+test("Roll die", () => {
+  let roller: Roller = new Roller(4); 
+  let actual:number=roller.roll(2); 
+  expect(actual).toBe(2);
+});
+
+test("Return last face of die", () => {
+  let roller: Roller = new Roller(4); 
+  let actual:number=roller.last(); 
+  expect(actual).toBe(0);
+});
+
--- src/Roller.ts
+++ src/Roller.ts
@@ -1,3 +1,4 @@
+
 export class Roller {
     private _distribution: Map<number, number>;
     private _last: number;
@@ -7,7 +8,10 @@ export class Roller {
     // so any invalid values should default to 6 to represent the common 
     // six-sided die.
     constructor(faces: number) {
-        faces;
+        if(faces>=2)
+            this._faces=faces;
+        else 
+            this._faces=6;
     }
 
     // specifies which number should be rolled and returns the recorded roll. 
@@ -21,13 +25,48 @@ export class Roller {
     //      key (value rolled) has an updated value that increases the number
     //      of rolls for that face by 1
     roll(value: number): number{
-        return value;
+        let flag=false;
+        if(value>=1 && value<=this._faces){
+            this._last=value;
+            // for (let i=0;i<this._distribution.size;i++){
+            //     if(this._distribution.get.first==value)
+            //         x.second+=1;
+            //     else
+            //         this._distribution.set(value,1);
+            // }
+            if(this._distribution){
+                this._distribution.forEach((val: number, key: number) => {
+                    //console.log(key, value);
+                    if(key==value){
+                        this._distribution.set(key,val+1);
+                        flag=true;
+                    }
+                        
+                        //val+=1;
+                    // else
+                    //     this._distribution.set(value,1);
+
+                });
+                if(flag==false)
+                    this._distribution.set(value,1);
+            }
+            // else
+            //     this._distribution.set(value,1);
+            return value;
+
+        }
+        else
+            return 0;
+            
     }
 
     // returns the value of the latest die roll. If no rolls have been made
     // yet, then it should return 0.
     last(): number {
-        return 0;
+        if(this._last>0)
+            return this._last;
+        else
+            return 0;
     }
 
     // returns a Map that represents the current distribution of rolls for each
